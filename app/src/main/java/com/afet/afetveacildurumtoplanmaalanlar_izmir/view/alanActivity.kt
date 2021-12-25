d7e13eb1cd748410d1f8a6165b276ea489e79f7e
Batuhan Umay <batuhanumay182@gmail.com>
2021-12-25 18:00:01 +0300

    Degisiklikler-2

--- app/src/main/java/com/afet/afetveacildurumtoplanmaalanlar_izmir/view/alanActivity.kt
+++ app/src/main/java/com/afet/afetveacildurumtoplanmaalanlar_izmir/view/alanActivity.kt
@@ -2,18 +2,35 @@ package com.afet.afetveacildurumtoplanmaalanlar_izmir.view
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.widget.Toast
+import androidx.lifecycle.Observer
+import androidx.lifecycle.ViewModel
+import androidx.lifecycle.ViewModelProvider
 import androidx.recyclerview.widget.LinearLayoutManager
 import com.afet.afetveacildurumtoplanmaalanlar_izmir.R
 import com.afet.afetveacildurumtoplanmaalanlar_izmir.adapter.AlanlarListAdapter
+import com.afet.afetveacildurumtoplanmaalanlar_izmir.viewmodel.AlanlarListViewModel
 import kotlinx.android.synthetic.main.activity_alan.*
 
 class alanActivity : AppCompatActivity() {
+
+    private var alanListViewModel = AlanlarListViewModel()
 
     var alanAdapter = AlanlarListAdapter()
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_alan)
+
+        alanListViewModel = ViewModelProvider(this).get(AlanlarListViewModel::class.java)
+
+        alanListViewModel.getAlanList()
+
+        alanListViewModel.gameListLiveData.observe(this, Observer { cevap->
+            Toast.makeText(applicationContext, cevap.kayitSayisi.toString() ,Toast.LENGTH_LONG).show() // obje için
+            //Toast.makeText(applicationContext, cevap[1].tur_ad, Toast.LENGTH_LONG).show() // api si list olanlar için
+
+        })
 
         var alanList : ArrayList<String> = arrayListOf()
         alanList.add("Poyrazköy")
